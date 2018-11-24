f5e87daf985ccc794b206d051c764a04a7e455dc
Mark <contact.mshin@gmail.com>
2018-11-23 17:17:38 -0700

    adding to routes

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,7 +1,23 @@
-var friends = require('friends');
+var friends = require('../data/friends.js');
 
 module.exports = function(app){
-    app.get('friends', function(req, res){
+    app.get('/api/friends', function(req, res){
         res.json(friends);
+    })
+
+    app.post('/api/friends', function(req, res){
+
+        var bestMatch = {
+            name: "",
+            image: "",
+            friendDifference: 1000
+        }
+
+        console.log(req.body);
+
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        console.log(userScores);
     })
 }
