35b7b5851d4a196deb6f68f5a22072265fdea0ab
Mark <contact.mshin@gmail.com>
2018-11-24 19:52:59 -0700

    layout change

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,52 @@
 var friends = [
     {
         name: "Sheldon Cooper",
-        image: "https://m.media-amazon.com/images/M/MV5BZjg4MGNlZDgtMmM5OC00ZmMxLTg3Y2EtZmZjOGJlNDU4NGNhXkEyXkFqcGdeQXVyMTgxOTIzNzk@._V1_UY666_CR259,0,666,666_AL_.jpg",
+        photo: "https://m.media-amazon.com/images/M/MV5BZjg4MGNlZDgtMmM5OC00ZmMxLTg3Y2EtZmZjOGJlNDU4NGNhXkEyXkFqcGdeQXVyMTgxOTIzNzk@._V1_UY666_CR259,0,666,666_AL_.jpg",
         scores: ["5", "5", "2", "1", "4", "3", "2", "3", "1", "2", "5", "3", "5", "4", "5"]
     },
     {
         name: "Penny",
-        image: "https://media.glamour.com/photos/5a0399bd8948116a5c05be65/master/w_644,c_limit/kaley-cuoco-the-big-bang-theory-penny-season-11-2017.jpg",
+        photo: "https://media.glamour.com/photos/5a0399bd8948116a5c05be65/master/w_644,c_limit/kaley-cuoco-the-big-bang-theory-penny-season-11-2017.jpg",
         scores: ["2", "1", "5", "4", "5", "2", "5", "2", "1", "2", "3", "4", "3", "5", "5"]
     },
     {
         name: "Bernadette Rostenkowski",
-        image: "https://vignette.wikia.nocookie.net/p__/images/9/95/Bernadette_Smiley_Face.jpeg/revision/latest?cb=20160313095524&path-prefix=protagonist",
+        photo: "https://vignette.wikia.nocookie.net/p__/images/9/95/Bernadette_Smiley_Face.jpeg/revision/latest?cb=20160313095524&path-prefix=protagonist",
         scores: ["2", "4", "3", "5", "1", "4", "3", "4", "2", "1", "5", "3", "1", "4", "2"]
     },
     {
         name: "Leonard Hofstadter",
-        image: "https://i.pinimg.com/originals/ec/24/80/ec24809dc360cd104cba747bc6a1c822.jpg",
+        photo: "https://i.pinimg.com/originals/ec/24/80/ec24809dc360cd104cba747bc6a1c822.jpg",
         scores: ["4", "3", "1", "3", "2", "1", "4", "5", "3", "2", "4", "1", "2", "4", "5"]
     },
     {
         name: "Amy Farrah Fowler",
-        image: "https://pbs.twimg.com/profile_images/2309494863/image.jpg",
+        photo: "https://pbs.twimg.com/profile_images/2309494863/image.jpg",
         scores: ["5", "4", "3", "2", "1", "5", "4", "3", "2", "1", "5", "4", "3", "1", "2"]
     },
     {
         name: "Raj Koothrappali",
-        image: "https://vignette.wikia.nocookie.net/tvdatabase/images/c/c2/Koothrappali.jpg/revision/latest?cb=20160901004307",
+        photo: "https://vignette.wikia.nocookie.net/tvdatabase/images/c/c2/Koothrappali.jpg/revision/latest?cb=20160901004307",
         scores: ["1", "2", "3", "2", "1", "4", "5", "4", "2", "1", "2", "3", "1", "4", "5"]
     },
     {
         name: "Howard Wolowitz",
-        image: "https://www.writeups.org/wp-content/uploads/Howard-Wolowitz-Big-Bang-Theory-Simon-Helberg-m.jpg",
+        photo: "https://www.writeups.org/wp-content/uploads/Howard-Wolowitz-Big-Bang-Theory-Simon-Helberg-m.jpg",
         scores: ["2", "3", "4", "5", "4", "1", "2", "3", "5", "2", "4", "1", "3", "2", "4"]
     },
     {
         name: "Stuart Bloom",
-        image: "https://vignette.wikia.nocookie.net/bigbangtheory/images/d/db/TCP-2.jpg/revision/latest?cb=20180411224247",
+        photo: "https://vignette.wikia.nocookie.net/bigbangtheory/images/d/db/TCP-2.jpg/revision/latest?cb=20180411224247",
         scores: ["1", "3", "2", "5", "4", "5", "4", "5", "3", "2", "1", "2", "1", "4", "5"]
     },
     {
         name: "Leslie Winkle",
-        image: "https://vignette.wikia.nocookie.net/thebigbangtheory/images/0/04/Leslie-winkle.jpeg/revision/latest?cb=20160125173232&path-prefix=pt-br",
+        photo: "https://vignette.wikia.nocookie.net/thebigbangtheory/images/0/04/Leslie-winkle.jpeg/revision/latest?cb=20160125173232&path-prefix=pt-br",
         scores: ["3", "4", "3", "2", "1", "5", "5", "4", "2", "1", "1", "4", "3", "5", "2"]
     },
     {
         name: "Barry Kripke",
-        image: "https://www.rp-assets.com/images/news/2018/04/17/41241-max.jpeg",
+        photo: "https://www.rp-assets.com/images/news/2018/04/17/41241-max.jpeg",
         scores: ["1", "5", "2", "4", "5", "2", "3", "2", "1", "2", "1", "3", "4", "3", "5"]
     }
 ]
