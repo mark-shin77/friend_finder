4b5863a44d7f5cd80b7968ca0aaab753a07edd21
Mark <contact.mshin@gmail.com>
2018-11-27 20:06:36 -0700

    styling

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ var PORT = process.env.PORT || 8080;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')));
 // parse various different custom JSON types as JSON
 app.use(bodyParser.json({ type: 'application/*+json' }))
 // parse some custom thing into a Buffer
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))    
 // parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
-
+    
 require("./app/routing/api-routes.js")(app);
 require("./app/routing/html-routes.js")(app);
 
